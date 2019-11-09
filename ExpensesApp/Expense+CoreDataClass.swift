7b22c5aa4530855f9f0b019a067d85dec0b0d4cd
jbkr53 <you@yourdomain.comjbkr53@mail.umkc.edu>
2019-11-08 21:27:43 -0600

    final

---
+++ ExpensesApp/Expense+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Expense+CoreDataClass.swift
+//  Expenses
+//
+//  Created by James Kirkland on 11/8/19.
+//  Copyright © 2019 Tech Innovator. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Expense)
+public class Expense: NSManagedObject {
+
+}
